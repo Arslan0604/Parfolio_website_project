7c46b0ec2e7765b0210185a59f92e312fab30204
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-28 19:39:04 +0500

    ars1

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Add event listener to navigation links
-document.querySelectorAll('nav ul li a').forEach((link) => {
-    link.addEventListener('click', (e) => {
+// scripts.js
+// Example: Smooth scroll effect for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        const id = link.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-    });
-});
 
-// Add event listener to contact form submit
-document.querySelector('.contact form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Add code here to send form data via email or API
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
